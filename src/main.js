a989058319d1ef9e843f77f01c076493a8f3c2c8
zhangwei <dflxzhang@163.com>
2020-08-28 13:55:13 +0800

    初始状态

--- src/main.js
+++ src/main.js
@@ -4,14 +4,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
+import axios from 'axios'
+import "element-ui/lib/theme-chalk/index.css"
 
 
+Vue.use(Element)
+Vue.prototype.$axios = axios //
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import UserLogin from '../views/UserLogin.vue'
+import Index from "../views/Index";
+import Register from "../views/Register";
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //     path: '/',
+    //     name: 'UserLogin',
+    //     component: UserLogin
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/Index',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/Register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
